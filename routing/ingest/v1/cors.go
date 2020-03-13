19ed740f3e36de90b610df8b652a6e490fa20dde
Shuaib Jewon <shuaib.jewon@slader.com>
2020-03-13 12:11:13 -0400

    Implementing v1 ingest handler

---
+++ routing/ingest/v1/cors.go
@@ -0,0 +1,16 @@
+package v1
+
+import "net/http"
+
+var AllowedHeaderOrigins = map[string]bool{}
+
+func setDefaultHeaders(w *http.ResponseWriter, req *http.Request) {
+	requestOrigin := req.Header.Get("Origin")
+	if AllowedHeaderOrigins[requestOrigin] {
+		(*w).Header().Set("Access-Control-Allow-Origin", requestOrigin)
+	}
+	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	(*w).Header().Set("Access-Control-Allow-Credentials", "true")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST OPTIONS")
+	(*w).Header().Set("Server", "Elasticsearch-Batcher/1.0")
+}
--- routing/ingest/v1/handler.go
+++ routing/ingest/v1/handler.go
@@ -1,7 +1,32 @@
 package v1
 
-import "net/http"
+import (
+	log "github.com/sirupsen/logrus"
+	"io/ioutil"
+	"net/http"
+	"shujew/elasticsearch-batcher/data/elasticsearch"
+)
 
 func Handler(w http.ResponseWriter, req *http.Request) {
-	//TODO: implement
+	setDefaultHeaders(&w, req)
+
+	switch req.Method {
+	case "POST":
+		POSTHandler(w, req)
+	case "OPTIONS":
+		w.WriteHeader(http.StatusOK)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func POSTHandler(w http.ResponseWriter, req *http.Request) {
+	if body, err := ioutil.ReadAll(req.Body); err != nil {
+		esClient := elasticsearch.GetBulkClient()
+		esClient.QueueForBulkIndexing(body)
+		w.WriteHeader(http.StatusCreated)
+	} else {
+		log.Error(err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
 }
