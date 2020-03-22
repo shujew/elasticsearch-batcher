4a1ecbbc0dc89ae32fab5798b91b16b3ca8b6b74
Shuaib Jewon <shuaib.jewon@slader.com>
2020-03-22 16:37:30 -0400

    Revert "Prefixing custom headers with X + ensuring they works w cors"
    This reverts commit 2735f702b73f5e4d3d295a8c69ccb97897740fac.

--- routing/ingestv1/cors.go
+++ routing/ingestv1/cors.go
@@ -22,9 +22,9 @@ func setDefaultHeaders(w *http.ResponseWriter, req *http.Request) {
 	if AllowAllOrigins || AllowedHeaderOrigins[requestOrigin] {
 		(*w).Header().Set("Access-Control-Allow-Origin", requestOrigin)
 	}
-	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type, X-Server-Name, X-Server-Unix-Timestamp")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	(*w).Header().Set("Access-Control-Allow-Credentials", "true")
 	(*w).Header().Set("Access-Control-Allow-Methods", "POST OPTIONS")
-	(*w).Header().Set("X-Server-Name", "Elasticsearch-Batcher/1.0")
-	(*w).Header().Set("X-Server-Unix-Timestamp", fmt.Sprintf("%d", time.Now().Unix()))
+	(*w).Header().Set("Server-Name", "Elasticsearch-Batcher/1.0")
+	(*w).Header().Set("Server-Unix-Timestamp", fmt.Sprintf("%d", time.Now().Unix()))
 }
